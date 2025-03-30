5d503f62b1ba29622363a820f6e272fa3f614898
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2025-03-30 21:33:39 +0700

    feat: add type, name output

--- app/modules/ec2_server/output.tf
+++ app/modules/ec2_server/output.tf
@@ -6,3 +6,11 @@ output "ec2_key_pair" {
 output "ec2_ipv4" {
   value = aws_eip.this.public_ip
 }
+
+output "ec2_tags" {
+  value = aws_instance.this.tags
+}
+
+output "ec2_instance_type" {
+  value = aws_instance.this.instance_type
+}
--- app/stacks/general/output.tf
+++ app/stacks/general/output.tf
@@ -1,7 +1,15 @@
-output "ec2_key_pair" {
+output "key_pair" {
   value = nonsensitive(module.general_server_1.ec2_key_pair)
 }
 
-output "ec2_ipv4" {
+output "ipv4" {
   value = module.general_server_1.ec2_ipv4
+}
+
+output "name" {
+  value = module.general_server_1.ec2_tags["Name"]
+}
+
+output "instance_type" {
+  value = module.general_server_1.ec2_instance_type
 }
